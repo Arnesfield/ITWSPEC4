b4f37e819b0e430ed2af651c02f540b56569b236
Arnesfield <rylee.jeff385@gmail.com>
2017-10-03 19:56:06 +0800

    Added initial project

---
+++ MP1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MP1
+//
+//  Created by Jefferson Rylee on 03/10/2017.
+//  Copyright © 2017 Jefferson Rylee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
