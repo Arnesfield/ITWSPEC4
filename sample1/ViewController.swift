3b918037ddecd32de74fa4a8fea251aae7a9ef6a
Jefferson Rylee <rylee.jeff385@gmail.com>
2017-10-03 16:13:24 +0800

    Added connection with components

--- sample1/ViewController.swift
+++ sample1/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtMsg: UITextField!
+    @IBOutlet weak var lblMsg: UILabel!
+    @IBAction func btnMsg(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
