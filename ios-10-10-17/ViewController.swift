e724d6af86a3bd2c5bb90f3eeb8cfa707d4318df
Jefferson Rylee <rylee.jeff385@gmail.com>
2017-10-10 15:47:20 +0800

    Added text field should return

--- ios-10-10-17/ViewController.swift
+++ ios-10-10-17/ViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var txtMsg: UITextField!
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        txtMsg.resignFirstResponder()
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
