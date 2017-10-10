950d339f2d021063d8743061a3c62ddca80efb1c
Jefferson Rylee <rylee.jeff385@gmail.com>
2017-10-10 16:49:20 +0800

    Final commit

--- ios-10-10-17/ViewController.swift
+++ ios-10-10-17/ViewController.swift
@@ -11,8 +11,19 @@ import UIKit
 class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var txtMsg: UITextField!
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    @IBOutlet weak var lblMsg: UILabel!
+    @IBAction func btnSubmit(_ sender: Any) {
+        lblMsg.text = txtMsg.text
         txtMsg.resignFirstResponder()
+    }
+    @IBAction func btnReset(_ sender: Any) {
+        txtMsg.text = ""
+        lblMsg.text = "Hello World"
+        txtMsg.resignFirstResponder()
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        lblMsg.text = txtMsg.text
+        textField.resignFirstResponder()
         return true
     }
     
