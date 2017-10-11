21ca7a27654cf9a10e9af5e41b342aeb69bbe223
Arnesfield <rylee.jeff385@gmail.com>
2017-10-11 14:45:17 +0800

    Added component connection

--- ios-10-10-17/ViewController.swift
+++ ios-10-10-17/ViewController.swift
@@ -10,6 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblRes: UILabel!
+    @IBAction func btnDot(_ sender: Any) {
+    }
+    @IBAction func btnZero(_ sender: Any) {
+    }
+    @IBAction func btnOne(_ sender: Any) {
+    }
+    @IBAction func btnTwo(_ sender: Any) {
+    }
+    @IBAction func btnThree(_ sender: Any) {
+    }
+    @IBAction func btnFour(_ sender: Any) {
+    }
+    @IBAction func btnFive(_ sender: Any) {
+    }
+    @IBAction func btnSix(_ sender: Any) {
+    }
+    @IBAction func btnSeven(_ sender: Any) {
+    }
+    @IBAction func btnEight(_ sender: Any) {
+    }
+    @IBAction func btnNine(_ sender: Any) {
+    }
+    
+    @IBAction func btnAdd(_ sender: Any) {
+    }
+    @IBAction func btnSubtract(_ sender: Any) {
+    }
+    @IBAction func btnMultiply(_ sender: Any) {
+    }
+    @IBAction func btnDivide(_ sender: Any) {
+    }
+    @IBAction func btnPercent(_ sender: Any) {
+    }
+    @IBAction func btnPlusMinus(_ sender: Any) {
+    }
+    @IBAction func btnDel(_ sender: Any) {
+    }
+    @IBAction func btnAC(_ sender: Any) {
+    }
+    
+    @IBAction func btnEquals(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
