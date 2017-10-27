091ca4160f0bb544a77e897f590c1306442f22b9
Jefferson Rylee <rylee.jeff385@gmail.com>
2017-10-27 16:38:25 +0800

    Added project

--- ios-10-10-17/ViewController.swift
+++ ios-10-10-17/ViewController.swift
@@ -8,8 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var lblRes: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return component == 1 ? self.operators.count : 10
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        // operators
+        if component == 1 {
+            return self.operators[row]
+        }
+        // numbers
+        else {
+            return String(row)
+        }
+    }
+    
+    private let operators = ["+", "-", "*", "÷", "%"]
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
