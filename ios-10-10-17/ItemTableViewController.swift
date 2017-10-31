d8e2f0fecf55dc3ab235813b01a696710a6f9400
Arnesfield <rylee.jeff385@gmail.com>
2017-11-01 02:13:36 +0800

    Added delete functionality

--- ios-10-10-17/ItemTableViewController.swift
+++ ios-10-10-17/ItemTableViewController.swift
@@ -87,17 +87,16 @@ class ItemTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            ItemTableViewController.ITEMS.remove(at: indexPath.row)
             // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
