c103989319685b0b5419f59b01b2f092aca3c414
Jefferson Rylee <rylee.jeff385@gmail.com>
2017-11-08 13:50:54 +0800

    Initial Commit

---
+++ BonusActivity1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BonusActivity1
+//
+//  Created by JEFFERSON RYLEE ESPIRITU on 08/11/2017.
+//  Copyright © 2017 iOS Arnesfield. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
