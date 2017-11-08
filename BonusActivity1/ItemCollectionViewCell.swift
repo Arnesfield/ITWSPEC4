c6d1d46956ece1dba5f6352f34248722f3ed2365
Jefferson Rylee <rylee.jeff385@gmail.com>
2017-11-08 14:57:00 +0800

    Update project

---
+++ BonusActivity1/ItemCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ItemCollectionViewCell.swift
+//  BonusActivity1
+//
+//  Created by JEFFERSON RYLEE ESPIRITU on 08/11/2017.
+//  Copyright © 2017 iOS Arnesfield. All rights reserved.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+}
