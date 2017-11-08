7929d1c7aa550ada3c987860c75b844e27f0443a
Jefferson Rylee <rylee.jeff385@gmail.com>
2017-11-08 16:24:52 +0800

    Updated project

--- ios-10-10-17/ViewController.swift
+++ ios-10-10-17/ViewController.swift
@@ -10,9 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var ball = BallView()
+    
+    @IBAction func tapHandler(_ sender: UITapGestureRecognizer) {
+        let point = sender.location(in: self.view)
+        UIView.animate(withDuration: 1.0) {
+            () -> Void in
+            self.ball.center = point
+        }
+    }
+    
+    var panCenterOffSet = CGPoint.zero
+    var isIgnorePan = false
+    
+    @IBAction func panHandler(_ sender: UIPanGestureRecognizer) {
+        switch (sender.state) {
+        case .began:
+            let point = sender.location(in: ball)
+            if (ball.containsPoints(point)) {
+                panCenterOffSet.x = ball.bounds.midX - point.x
+                panCenterOffSet.y = ball.bounds.midY - point.y
+            }
+            else {
+                isIgnorePan = true
+            }
+        case .changed:
+            if !isIgnorePan {
+                let panLocation = sender.location(in: self.view)
+                ball.center = CGPoint(x: panLocation.x + panCenterOffSet.x, y: panLocation.y + panCenterOffSet.y)
+            }
+        case .ended:
+            isIgnorePan = false
+        default: break
+        }
+    }
+    @IBAction func pinchHandler(_ sender: UIPinchGestureRecognizer) {
+        switch (sender.state) {
+        case .changed:
+            ball.transform = CGAffineTransform(scaleX: sender.scale, y: sender.scale)
+        case .ended:
+            let newBallRect = ball.bounds.applying(ball.transform)
+            ball.bounds = newBallRect
+            ball.setNeedsDisplay()
+        default: break
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        ball.frame = CGRect(
+            x: view.bounds.midX - 50,
+            y: view.bounds.midY - 50,
+            width: 100,
+            height: 100)
+        ball.backgroundColor = UIColor.clear
+        ball.isOpaque = true
+        self.view.addSubview(ball)
     }
 
     override func didReceiveMemoryWarning() {
