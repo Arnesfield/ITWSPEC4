2989d7dcece39999d665f82fedada419d4ddcb8a
Arnesfield <rylee.jeff385@gmail.com>
2017-11-09 22:59:36 +0800

    Updated functionality, added UserDefaults

--- temp/ViewController.swift
+++ temp/ViewController.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var textColor = [Float]()
-    var bgColor = [Float]()
+    var textColor: [Float] = [1, 1, 1]
+    var bgColor: [Float] = [0, 0, 0]
     
     @IBOutlet weak var rSliderOutlet: UISlider!
     @IBOutlet weak var gSliderOutlet: UISlider!
@@ -18,6 +18,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var switchOutlet: UISwitch!
     @IBOutlet weak var field: UITextField!
     @IBOutlet weak var lblMsg: UILabel!
+    @IBOutlet weak var lblBgStats: UILabel!
+    @IBOutlet weak var lblTextStats: UILabel!
+    @IBOutlet weak var lblTextSwitch: UILabel!
+    @IBOutlet weak var lblBgSwitch: UILabel!
+    @IBOutlet weak var lblOwner: UILabel!
     @IBAction func btnShowMsg(_ sender: Any) {
         lblMsg.text = field.text
     }
@@ -53,6 +58,7 @@ class ViewController: UIViewController {
             bgColor[2] = bSliderOutlet.value
         
             setBgColor()
+            setStats(for: lblBgStats, colors: bgColor, name: "Background")
         }
         else {
             textColor[0] = rSliderOutlet.value
@@ -60,49 +66,109 @@ class ViewController: UIViewController {
             textColor[2] = bSliderOutlet.value
         
             setTextColor()
+            setStats(for: lblTextStats, colors: textColor, name: "Text")
         }
+        
+        // saveStates()
     }
+    
+    func render() -> Void {
+        setTextColor()
+        setBgColor()
+        setStats(for: lblBgStats, colors: bgColor, name: "Background")
+        setStats(for: lblTextStats, colors: textColor, name: "Text")
+        
+        // saveStates()
+    }
+    
     func reset() -> Void {
         field.text = ""
         lblMsg.text = "Hello World!"
         switchOutlet.isOn = true
         
-        for i in 0..<3 {
-            textColor[i] = 255
-            bgColor[i] = 0
-        }
+        textColor = [1, 1, 1]
+        bgColor = [0, 0, 0]
         
-        setTextColor()
-        setBgColor()
+        render()
     }
+    
+    
     func setTextColor() -> Void {
-        lblMsg.textColor = UIColor.init(red: CGFloat(textColor[0]), green: CGFloat(textColor[1]), blue: CGFloat(textColor[2]), alpha: 1.0)
+        let color = self.getColor(textColor)
         
-        rSliderOutlet.setValue(textColor[0], animated: true)
-        gSliderOutlet.setValue(textColor[1], animated: true)
-        bSliderOutlet.setValue(textColor[2], animated: true)
+        lblMsg.textColor = color
+        lblBgStats.textColor = color
+        lblTextStats.textColor = color
+        lblBgSwitch.textColor = color
+        lblTextSwitch.textColor = color
+        lblOwner.textColor = color
+        
+        self.setSliderOutlets(textColor)
     }
     func setBgColor() -> Void {
-        self.view.backgroundColor = UIColor.init(red: CGFloat(bgColor[0]), green: CGFloat(bgColor[1]), blue: CGFloat(bgColor[2]), alpha: 1.0)
+        self.view.backgroundColor = self.getColor(bgColor)
         
-        rSliderOutlet.setValue(bgColor[0], animated: true)
-        gSliderOutlet.setValue(bgColor[1], animated: true)
-        bSliderOutlet.setValue(bgColor[2], animated: true)
+        self.setSliderOutlets(bgColor)
     }
+    
+    func getColor(_ arr: [Float]) -> UIColor {
+        return UIColor.init(red: CGFloat(arr[0]), green: CGFloat(arr[1]), blue: CGFloat(arr[2]), alpha: 1.0)
+    }
+    
+    func setSliderOutlets(_ arr: [Float]) -> Void {
+        rSliderOutlet.setValue(arr[0], animated: true)
+        gSliderOutlet.setValue(arr[1], animated: true)
+        bSliderOutlet.setValue(arr[2], animated: true)
+    }
+    
+    func setStats(for label: UILabel, colors: [Float], name: String) -> Void {
+        let red = Int(colors[0] * 255)
+        let green = Int(colors[1] * 255)
+        let blue = Int(colors[2] * 255)
+        label.text = "\(name) = Red: \(red), Green: \(green), Blue: \(blue)"
+    }
+    
+    func saveStates() -> Void {
+        defaults.set(bgColor, forKey: "bgColor")
+        defaults.set(textColor, forKey: "textColor")
+        defaults.set(switchOutlet.isOn, forKey: "switch")
+        defaults.set(field.text, forKey: "field")
+        defaults.set(lblMsg.text, forKey: "lblMsg")
+    }
+    
+    let defaults = UserDefaults.standard
     
     // override
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        textColor.append(0.0)
-        textColor.append(0.0)
-        textColor.append(0.0)
         
-        bgColor.append(0.0)
-        bgColor.append(0.0)
-        bgColor.append(0.0)
+        if let sBgColor = defaults.object(forKey: "bgColor"),
+            let sTextColor = defaults.object(forKey: "textColor"),
+            let sSwitch = defaults.object(forKey: "switch"),
+            let sField = defaults.object(forKey: "field"),
+            let sLblMsg = defaults.object(forKey: "lblMsg")
+        {
+            bgColor = sBgColor as! [Float]
+            textColor = sTextColor as! [Float]
+            switchOutlet.isOn = sSwitch as! Bool
+            field.text = sField as? String
+            lblMsg.text = sLblMsg as? String
+            
+            render()
+            setSliderOutlets(switchOutlet.isOn ? bgColor : textColor)
+        }
+        else {
+            reset()
+        }
         
-        reset()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        saveStates()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        // saveStates()
     }
 
     override func didReceiveMemoryWarning() {
